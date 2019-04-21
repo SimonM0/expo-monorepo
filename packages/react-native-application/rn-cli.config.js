4b58fba7bcd5b6d4dbf383bfd7133ec39c01e0ac
Lucas Munhoz <ln.munhoz@gmail.com>
2019-04-21 19:17:26 +0700

    Add updated expo dependencies and config workspaces

--- packages/react-native-application/rn-cli.config.js
+++ packages/react-native-application/rn-cli.config.js
@@ -1,15 +1,3 @@
-const path = require('path');
+const { createReactNativeConfiguration } = require('expo-yarn-workspaces');
 
-const config = {
-  getProjectRoots() {
-    return [
-      // Resolves the project directory.
-      path.resolve(__dirname),
-
-      // Resolves the monorepo's base folder
-      path.resolve(__dirname, '../..'),
-    ];
-  },
-};
-
-module.exports = config;
+module.exports = createReactNativeConfiguration(__dirname);
